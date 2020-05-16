4d7332e65b49f8af580790be6aae43a937671152
panospd <anastapd@gmail.com>
2020-05-16 12:52:11 +0100

    Implemented basic view of landing page

---
+++ pages/about/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="about-page">
+    <h1>About me</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem, et. </p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas error necessitatibus neque!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis!</p>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe, esse?</p>
+  </div>
+</template>
+
+<style scoped>
+  .about-page {
+    text-align: center;
+    padding: 30px;
+  }
+</style>
